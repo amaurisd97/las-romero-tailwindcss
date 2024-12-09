e068fa96731a6944696e53674372102ee78c4b5c
AmaurisD <amaurisd97@gmail.com>
2024-12-09 05:01:36 -0500

    added main tag

--- app/page.js
+++ app/page.js
@@ -6,12 +6,12 @@ import Services from '@/components/services'
 
 export default function Home() {
 	return (
-		<>
+		<main>
 			<NavBar />
 			<Header />
 			<ShowUp />
 			<AboutUs />
 			<Services />
-		</>
+		</main>
 	)
 }
