d319ecac074ad335f57a2226056134e760a25780
AmaurisD <amaurisd97@gmail.com>
2024-12-09 04:04:28 -0500

    Improve SEO by adding descriptive alt text, enhancing metadata, and creating robots.txt

--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import Header from '@/components/header-test'
 import NavBar from '@/components/nav-bar'
 import ShowUp from '@/components/show-up'
@@ -8,68 +7,11 @@ import Services from '@/components/services'
 export default function Home() {
 	return (
 		<>
-			<Head>
-				<title>Las Romero - Home</title>
-				<meta
-					name='description'
-					content='Welcome to Las Romero, your premier destination for high-quality services. Discover our strategic location, immediate and efficient service, and comfortable, air-conditioned rooms.'
-				/>
-				<meta
-					name='keywords'
-					content='Las Romero, accommodation, services, strategic location, efficient service, comfortable rooms, tour guide, classic car tours, beverages and food'
-				/>
-				<meta
-					name='author'
-					content='Your Name or Company'
-				/>
-				<meta
-					property='og:title'
-					content='Las Romero - Home'
-				/>
-				<meta
-					property='og:description'
-					content='Welcome to Las Romero, your premier destination for high-quality services. Discover our strategic location, immediate and efficient service, and comfortable, air-conditioned rooms.'
-				/>
-				<meta
-					property='og:image'
-					content='/public/header-bg.png'
-				/>
-				<meta
-					property='og:url'
-					content='https://yourdomain.com'
-				/>
-				<meta
-					property='og:type'
-					content='website'
-				/>
-				<meta
-					property='og:site_name'
-					content='Las Romero'
-				/>
-				<script type='application/ld+json'>
-					{`
-						{
-							"@context": "https://schema.org",
-							"@type": "WebSite",
-							"url": "https://yourdomain.com",
-							"name": "Las Romero",
-							"description": "Welcome to Las Romero, your premier destination for high-quality services. Discover our strategic location, immediate and efficient service, and comfortable, air-conditioned rooms.",
-							"potentialAction": {
-								"@type": "SearchAction",
-								"target": "https://yourdomain.com/search?q={search_term_string}",
-								"query-input": "required name=search_term_string"
-							}
-						}
-					`}
-				</script>
-			</Head>
-			<>
-				<NavBar />
-				<Header />
-				<ShowUp />
-				<AboutUs />
-				<Services />
-			</>
+			<NavBar />
+			<Header />
+			<ShowUp />
+			<AboutUs />
+			<Services />
 		</>
 	)
 }
